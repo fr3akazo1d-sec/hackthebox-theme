265c86f88f546b7a7cd9b22cae11ebc21bd1da2b
Ph1l M <mallephil@gmail.com>
2022-03-27 13:50:33 +0200

    Delete powersehll directory

--- powersehll/Microsoft.PowerShell_profile.ps1
+++
@@ -1,6 +0,0 @@
-function prompt
-{
-    Write-Host ("`e[1;32m┌─" + "`e[32m[`e[37m" + "`e[32m`e[34m" + (hostname) + "`e[32m]─[`e[37m" + (Get-Location) + "`e[32m]")
-    Write-Host ("`e[32m└──╼ [`e[34mPS`e[32m]>") -nonewline
-    return " "
-}
